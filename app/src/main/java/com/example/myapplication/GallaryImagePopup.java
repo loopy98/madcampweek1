aef376dbe977d0fb0c32495059a2ae7f0c040d54
LSJ <junjlim8513@gmail.com>
2018-12-31 21:48:31 +0900

    Halligalli ~ing

--- app/src/main/java/com/example/myapplication/GallaryImagePopup.java
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication;
-
-import android.app.Activity;
-import android.view.View;
-
-public class GallaryImagePopup extends Activity implements View.OnClickListener {
-
-}
