69b94fd7d0bdc76ba2a58a461d4db879d6739e9f
LSJ <junjlim8513@gmail.com>
2019-01-01 18:42:25 +0900

    Halligalli finished

--- app/src/main/java/com/example/myapplication/HalligalliActivity.java
+++ app/src/main/java/com/example/myapplication/HalligalliActivity.java
@@ -14,17 +14,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
 import java.util.Arrays;
 import java.util.Random;
 
 public class HalligalliActivity extends AppCompatActivity {
+    private long backPressedTime = 0;
+    private final long FINISH_INTERVAL_TIME = 2000;
     int player1_score = 0;
     int player2_score = 0;
     int current_score = 0;
     int random_num;
     boolean is_tapped = false;
+    boolean stop_main = false;
+    GridView gridview;
+    ImageAdapter imageadapter;
     DisplayMetrics mMetrics = new DisplayMetrics();
-    GridView gridview = (GridView) findViewById(R.id.halligalli_gridview);
     Integer[] halligalli_list = {R.drawable.banana1, R.drawable.banana2, R.drawable.banana3, R.drawable.banana4, R.drawable.banana5,
             R.drawable.jadu1, R.drawable.jadu2, R.drawable.jadu3, R.drawable.jadu4, R.drawable.jadu5,
             R.drawable.melon1, R.drawable.melon2, R.drawable.melon3, R.drawable.melon4, R.drawable.melon5,
@@ -51,7 +57,9 @@ public class HalligalliActivity extends AppCompatActivity {
     }
 
     public void halligalli_main(){
-        if ( player1_score >= 100 ) {
+        if (stop_main) return;
+        stop_main = true;
+        if ( player1_score >= 50 ) {
             setContentView(R.layout.halligalli_victory);
             TextView who = findViewById(R.id.halligalli_victory);
             String who_wins = "Player 1 Wins!!";
@@ -67,7 +75,7 @@ public class HalligalliActivity extends AppCompatActivity {
                 }
             }, 3000);
         }
-        else if (player2_score >= 100){
+        else if (player2_score >= 50){
             setContentView(R.layout.halligalli_victory);
             TextView who = findViewById(R.id.halligalli_victory);
             String who_wins = "Player 2 Wins!!";
@@ -97,40 +105,103 @@ public class HalligalliActivity extends AppCompatActivity {
             current_list[current_score%4] = new_card;
             current_score++;
 
-            gridview.setAdapter(new ImageAdapter(this));
-        }
-    }
-
-    public void halligalli_start(View v){
-        gridview.setAdapter(new ImageAdapter(this));
-        halligalli_main();
-    }
-
-    public void halligalli_home(View v) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    public void tab1(View v) {
-        if ( (fruit_num[0] == 5) || (fruit_num[1] == 5) || (fruit_num[2] == 5) || (fruit_num[3] == 5)) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Good Job!!", Toast.LENGTH_SHORT);
-            toast.show();
-
-            player1_score += current_score;
-            current_score = 0;
-            for (int i=0; i<4; i++) {
-                current_list[i] = R.drawable.white;
-                fruit_num[i] = 0;
-            }
+            imageadapter = new ImageAdapter(this);
+            gridview = (GridView) findViewById(R.id.halligalli_gridview);
+            gridview.setAdapter(imageadapter);
+//            imageadapter.notifyDataSetChanged();
 
             Handler delayHandler = new Handler();
             delayHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    halligalli_main();
+                    if (!is_tapped) {
+                        stop_main = false;
+                        halligalli_main();
+                    }
                 }
-            }, 1500);
+            }, 1000);
+        }
+    }
+
+    public void halligalli_start(View v){
+        gridview = (GridView) findViewById(R.id.halligalli_gridview);
+        imageadapter = new ImageAdapter(this);
+        gridview.setAdapter(imageadapter);
+        if (is_tapped) {
+            stop_main = false;
+            is_tapped = false;
+        }
+        halligalli_main();
+    }
+
+//    public void halligalli_home(View v) {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+
+    public void tap1(View v) {
+        if (is_tapped) return;
+        is_tapped = true;
+        stop_main = true;
+        if ( (fruit_num[0] == 5) || (fruit_num[1] == 5) || (fruit_num[2] == 5) || (fruit_num[3] == 5)) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Player 1 Good Job!!", Toast.LENGTH_SHORT);
+            toast.show();
+
+            player1_score += current_score;
+            setScore();
+            current_score = 0;
+            for (int i=0; i<4; i++) {
+                current_list[i] = R.drawable.white;
+                fruit_num[i] = 0;
+            }
+        }
+        else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Oops!!", Toast.LENGTH_SHORT);
+            toast.show();
+
+            player1_score = ( (player1_score >= 5) ? player1_score-5 : 0 );
+            setScore();
+        }
+    }
+
+    public void tap2(View v) {
+        if (is_tapped) return;
+        is_tapped = true;
+        stop_main = true;
+        if ( (fruit_num[0] == 5) || (fruit_num[1] == 5) || (fruit_num[2] == 5) || (fruit_num[3] == 5)) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Player 2 Good Job!!", Toast.LENGTH_SHORT);
+            toast.show();
+
+            player2_score += current_score;
+            setScore();
+            current_score = 0;
+            for (int i=0; i<4; i++) {
+                current_list[i] = R.drawable.white;
+                fruit_num[i] = 0;
+            }
+        }
+        else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Oops!!", Toast.LENGTH_SHORT);
+            toast.show();
+
+            player2_score = ( (player2_score >= 5) ? player2_score-5 : 0 );
+            setScore();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - backPressedTime;
+
+        if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime) {
+            super.onBackPressed();
+        } else {
+            backPressedTime = tempTime;
+            Intent it = new Intent(this, MainActivity.class);
+            startActivity(it);
+            finish();
         }
     }
 
@@ -156,18 +227,20 @@ public class HalligalliActivity extends AppCompatActivity {
         // create a new ImageView for each item referenced by the Adapter
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            int height = (mMetrics.heightPixels) / 2;
+//            int height = (mMetrics.heightPixels) / 2;
 
             ImageView imageView;
             if (convertView == null) {
                 imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams((int)(0.66*((float)height)), height));
+                imageView.setLayoutParams(new GridView.LayoutParams(300, 450));
+//                imageView.setLayoutParams(new GridView.LayoutParams((int)(0.66*((float)height)), height));
                 imageView.setScaleType(ImageView.ScaleType.FIT_XY);
                 imageView.setPadding(3, 3, 3, 3);
             } else {
                 imageView = (ImageView) convertView;
             }
-            imageView.setImageResource(current_list[position]);
+//            imageView.setImageResource(current_list[position]);
+            Glide.with(mContext).load(current_list[position]).into(imageView);
             return imageView;
         }
     }
